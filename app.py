2f1579ef4ee9560415a0e99283950d106f1098a3
Alicia-09 <24308060610613@cetis61.edu.mx>
2026-04-20 06:56:11 -0500

    diseño gestor

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("login.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/registro")
+def registro():
+    return render_template("registro.html")
+
+@app.route("/inicio")
+def inicio():
+    return render_template("inicio.html")
+
+@app.route("/logout")
+def logout():
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
